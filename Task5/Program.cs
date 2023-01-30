aa4a581b6e9eb495d1eca6735058be905d97ab09
gunderak <sergey.gund@gmail.com>
2023-01-30 21:24:29 +0300

    add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+//  5, 25  ->  да
+//  -4, 16  ->  да
+//  25, 5  ->  да
+//  8,9  ->  нет
+
+Console.Clear();
+
+Console.WriteLine("Введите число 1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine($"{num1}, {num2} -> да");
+}
+else
+{
+    Console.WriteLine($"{num1}, {num2} -> нет");
+}
